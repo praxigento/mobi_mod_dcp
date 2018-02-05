ca0096f85f3be440a79650d722190cff776d1b8e
Alex Gusev <alex@flancer64.com>
2018-02-05 21:13:06 +0200

    MOBI-1131 Extract DCP func from Hybrid module

---
+++ Config.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Module's configuration (hard-coded).
+ *
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Dcp;
+
+class Config
+    extends \Praxigento\BonusReferral\Config
+{
+    const MODULE = 'Praxigento_Dcp';
+}
