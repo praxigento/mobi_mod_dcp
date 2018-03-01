e3b764d74df47d97ce83790c1cb975ffa963af47
Alex Gusev <alex@flancer64.com>
2018-03-02 00:08:33 +0200

    MOBI-1158 Referral link page on front

---
+++ Controller/Index/Index.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * User: Alex Gusev <alex@flancer64.com>
+ */
+
+namespace Praxigento\Dcp\Controller\Index;
+
+class Index
+    extends \Magento\Framework\App\Action\Action
+{
+    public function execute()
+    {
+        $type = \Magento\Framework\Controller\ResultFactory::TYPE_PAGE;
+        /** @var \Magento\Framework\View\Result\Page $resultPage */
+        $resultPage = $this->resultFactory->create($type);
+        $resultPage->getConfig()->getTitle()->set(__('Downline Control Panel'));
+        return $resultPage;
+    }
+
+}
